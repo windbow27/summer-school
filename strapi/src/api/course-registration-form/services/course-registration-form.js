8ab932d37d17595ffddd0895cf2fd9eb8518854c
cheesedz <chisnghia@gmail.com>
2024-06-04 23:22:45 +0700

    added content-type

---
+++ strapi/src/api/course-registration-form/services/course-registration-form.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * course-registration-form service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::course-registration-form.course-registration-form');
